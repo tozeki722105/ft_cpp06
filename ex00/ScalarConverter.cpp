086e783f6d2c2967d1802d732b95adb7d16878c1
Toshihiro Ozeki <tozeki@c1r6s8.42tokyo.jp>
2024-12-14 21:38:45 +0900

    fix: 出力がおかしいのを一部修正(まだ修正する箇所は残っている)

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -16,6 +16,14 @@ bool isNanOrInf(const std::string &str)
 	return str == "nan" || str == "+inf" || str == "-inf";
 }
 
+void putNanOrInf(const std::string &str)
+{
+	std::cout << "char: impossible\n";
+	std::cout << "int: impossible\n";
+	std::cout << "float: " << str << "f\n";
+	std::cout << "double: " << str << "\n";
+}
+
 void ScalarConverter::typePutChar(const std::string &str)
 {
 	char val = str[0];
@@ -25,9 +33,9 @@ void ScalarConverter::typePutChar(const std::string &str)
 	// int
 	std::cout << "int: " << static_cast<int>(val) << "\n";
 	// float
-	std::cout << "float: " << val << ".0f\n";
+	std::cout << "float: " << static_cast<float>(val) << ".0f\n";
 	// double
-	std::cout << "double: " << val << ".0f\n";
+	std::cout << "double: " << static_cast<double>(val) << ".0\n";
 }
 
 void ScalarConverter::typePutInt(const std::string &str)
@@ -45,29 +53,32 @@ void ScalarConverter::typePutInt(const std::string &str)
 	// int
 	std::cout << "int: " << static_cast<int>(val) << "\n";
 	// float
-	if (isOverFlow<int, float>(val))
+	if (isOverFlow<int>(val)) //判定する方がfloat
 		std::cout << "float: impossible\n";
 	else
-		std::cout << "float: " << val << ".0f\n";
+		std::cout << "float: " << static_cast<float>(val) << ".0\n";
 	// double
-	std::cout << "double: " << val << ".0f\n";
+	std::cout << "double: " << static_cast<double>(val) << ".0\n";
 }
 
 void ScalarConverter::typePutFloat(const std::string &str)
 {
 	const_cast<std::string &>(str).resize(str.length() - 1);
+	if (isNanOrInf(str))
+		return putNanOrInf(str);
+
 	std::istringstream iss(str);
 	float val;
 	if (!(iss >> val))
 		throw std::invalid_argument("Conversion failed: Float");
 
 	// char
-	if (isNanOrInf(str) || isOverFlow<float, char>(val) || !isInt(val))
+	if (isOverFlow<float, char>(val) || !isInt(val))
 		std::cout << "char: impossible\n";
 	else
 		putCharPrintable(static_cast<int>(val));
 	// int
-	if (isNanOrInf(str) || isOverFlow<float, int>(val) || !isInt(val))
+	if (isOverFlow<float, int>(val) || !isInt(val))
 		std::cout << "int: impossible\n";
 	else
 		std::cout << "int: " << static_cast<int>(val) << "\n";
@@ -78,40 +89,43 @@ void ScalarConverter::typePutFloat(const std::string &str)
 		std::cout << "float: " << val << "f\n";
 	// double
 	if (isInt(val))
-		std::cout << "double: " << val << ".0f\n";
+		std::cout << "double: " << static_cast<double>(val) << ".0\n";
 	else
-		std::cout << "double: " << val << "f\n";
+		std::cout << "double: " << static_cast<double>(val) << "\n";
 }
 
 void ScalarConverter::typePutDouble(const std::string &str)
 {
+	if (isNanOrInf(str))
+		return putNanOrInf(str);
+	
 	std::istringstream iss(str);
 	double val;
 	if (!(iss >> val))
 		throw std::invalid_argument("Conversion failed: Double");
 
 	// char
-	if (isNanOrInf(str) || isOverFlow<double, char>(val) || !isInt(val))
+	if (isOverFlow<double, char>(val) || !isInt(val))
 		std::cout << "char: impossible\n";
 	else
 		putCharPrintable(static_cast<int>(val));
 	// int
-	if (isNanOrInf(str) || isOverFlow<double, int>(val) || !isInt(val))
+	if (isOverFlow<double, int>(val) || !isInt(val))
 		std::cout << "int: impossible\n";
 	else
 		std::cout << "int: " << static_cast<int>(val) << "\n";
 	// float
-	if (isOverFlow<float>(val))
+	if (isOverFlow<double>(val)) //判定する方がfloat
 		std::cout << "float: impossible\n";
 	else if (isInt(val))
-		std::cout << "float: " << val << ".0f\n";
+		std::cout << "float: " << static_cast<float>(val) << ".0f\n";
 	else
-		std::cout << "float: " << val << "f\n";
+		std::cout << "float: " << static_cast<float>(val) << "f\n";
 	// double
 	if (isInt(val))
-		std::cout << "double: " << val << ".0f\n";
+		std::cout << "double: " << val << ".0\n";
 	else
-		std::cout << "double: " << val << "f\n";
+		std::cout << "double: " << val << "\n";
 }
 
 /// @param str must not empty
@@ -156,11 +170,9 @@ ScalarConverter::e_type ScalarConverter::getType(const std::string &str)
 	//  numeric type
 	std::string::size_type e_pos = str.find('e');
 	if (e_pos == std::string::npos)
-		return (getNumericType(str));
-	if (str.rfind('e') != e_pos)  // eが複数ある場合
-		return ERROR;
+		return getNumericType(str);
+	bool fFlag = (str.find('f') == str.length() - 1);  // fが最後尾に存在する場合 ex)42e3f
 	std::string mantissa = str.substr(0, e_pos);
-	bool fFlag = (str.find('f') == str.length() - 1);  // fが最後尾に存在する場合　ex)42e3f
 	std::string exponent = str.substr(e_pos + 1, str.length() - e_pos - 1 - fFlag);
 	e_type mantissaType = getNumericType(mantissa);
 	if (mantissaType == ERROR || mantissaType == FLOAT  // ex)42fe1,42fe2f
@@ -171,6 +183,8 @@ ScalarConverter::e_type ScalarConverter::getType(const std::string &str)
 
 void ScalarConverter::convert(const std::string &str)
 {
+	typePutDouble(str);
+	return;
 	switch (getType(str)) {
 		case CHAR:
 			typePutChar(str);
